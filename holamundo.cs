43d8a717101d5e42c531cf273ef80f595caecb5f
MIguel Eve <mevealcantara@gmail.com>
2025-08-14 18:43:55 -0400

    Create holamundo.cs

---
+++ holamundo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mensaje de bienvenida
+            Console.WriteLine("¡Hola mundo!");
+        }
+    }
+}
